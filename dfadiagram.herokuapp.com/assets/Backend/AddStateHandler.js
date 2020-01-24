4cfd9dc43b30fbecc0e7f31506fb1e9f27e65cd7
Daniel Menjivar <d.menjivar@outlook.com>
2020-01-24 10:17:09 -0800

    Squashed bug that caused states to reprint

--- dfadiagram.herokuapp.com/assets/Backend/AddStateHandler.js
+++ dfadiagram.herokuapp.com/assets/Backend/AddStateHandler.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
             DFATuples.state = ['q1'];
             DFATuples.initial = ['q1'];
             subesh.map(DFATuples);
-            drawer.createStateCircle();
+            drawer.createDiagram();
             redraw();
         } else { //there exists at least 1 state in the fsm
             console.log(`Had ${subesh.state.length} states`)
             DFATuples.state.push(`q${DFATuples.state.length + 1}`);
             subesh.map(DFATuples);
-            drawer.createStateCircle();
+            drawer.createDiagram();
             redraw();
         }
     });
