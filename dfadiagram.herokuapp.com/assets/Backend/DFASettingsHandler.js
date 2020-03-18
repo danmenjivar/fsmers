3557e00a0cd00873b92c8d7044361dee8d18b61d
Daniel Menjivar <d.menjivar@outlook.com>
2020-03-18 13:38:19 -0700

    Fixed bug where alphabet values weren't being updated when changed.

--- dfadiagram.herokuapp.com/assets/Backend/DFASettingsHandler.js
+++ dfadiagram.herokuapp.com/assets/Backend/DFASettingsHandler.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
     DFATuples.state = $('#state').val().split(',');
     DFATuples.initial = $('#initial').val().split(',', 1);
     DFATuples.final = $('#final').val().split(',');
-    DFATuples.albhabet = $('#alphabet').val().split(',');
+    DFATuples.alphabet = $('#alphabet').val().split(',');
 
     let start = DFATuples.state.find(item => item == DFATuples.initial);
     let final = DFATuples.final.every((final) => {
