7479b2d74c77edc3bc1182129a1b403010ae7ce6
Daniel Menjivar <d.menjivar@outlook.com>
2020-03-18 10:47:06 -0700

    Refactored delayLoop into a setInterval funtion, works the same but easier to read. Also: discovered 0-day game breaking bug. If a string begins execution before another string has finished processing, entire engine breaks. Must create an event handler to trigger function instead of relying on processing delay.

--- dfadiagram.herokuapp.com/assets/Backend/BatchTestingHandler.js
+++ dfadiagram.herokuapp.com/assets/Backend/BatchTestingHandler.js
@@ -9,28 +9,44 @@ $(document).ready(function () {
     let i = 0;
     let strprn = document.getElementById("curStr");
     let precheck = preChecker(strings);
-    if(precheck != true){
+    if (precheck != true) {
       alert(`The following string(s) contain characters not in the alphabet: \n${precheck}\nPlease either expand the language or remove these strings to begin testing.`)
       return;
     }
     clearResultsTable();
-    function delayLoop() {
-      setTimeout(function () {
-        strprn.innerHTML = `<h2>${strings[i]}<\h2>`;
-        if (i + 1 == strings.length){
-          checker.finalCheck = true;
-        }
-        checker.check(strings[i]);
-        i++;
-        if (i < strings.length) {
-          delayLoop();
-        } else {
-          evenOutResults();
-          updateTimeStamp(Date.now());
-        }
-      }, delay);
-    }
-    delayLoop();
+
+    let testingInterval = setInterval(function () {
+      strprn.innerHTML = `<h2>${strings[i]}<\h2>`;
+      if (i + 1 == strings.length) {
+        checker.finalCheck = true;
+      }
+      checker.check(strings[i]);
+      i++;
+      if (i >= strings.length) {
+        clearInterval(testingInterval);
+        evenOutResults();
+        updateTimeStamp(Date.now());
+      }
+    }, delay);
+
+
+    // function delayLoop() {
+    //   setTimeout(function () {
+    //     strprn.innerHTML = `<h2>${strings[i]}<\h2>`;
+    //     if (i + 1 == strings.length){
+    //       checker.finalCheck = true;
+    //     }
+    //     checker.check(strings[i]);
+    //     i++;
+    //     if (i < strings.length) {
+    //       delayLoop();
+    //     } else {
+    //       evenOutResults();
+    //       updateTimeStamp(Date.now());
+    //     }
+    //   }, delay);
+    // }
+    // delayLoop();
     $('#inputModal').modal('hide');
     $('#navCollapseBut').trigger('click');
   });
@@ -42,8 +58,8 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 //Slider Handlers
@@ -65,29 +81,29 @@ function clearResultsTable() {
   $('#tableResults tbody').html("<tr></tr><tr></tr>");
 }
 
-function preChecker(strings){
+function preChecker(strings) {
   let badStrings = [];
   let alphaRegex = new RegExp(`[^${subesh.alphabet.join("")}]+`);
 
-  for(let i = 0; i < strings.length; i++){
-    if(alphaRegex.test(strings[i])){
+  for (let i = 0; i < strings.length; i++) {
+    if (alphaRegex.test(strings[i])) {
       badStrings.push(strings[i]);
     }
   }
 
-  if (badStrings.length === 0){
+  if (badStrings.length === 0) {
     return true;
   } else {
     return badStrings.join(',');
   }
 }
 
-function updateTimeStamp(timestmp){
+function updateTimeStamp(timestmp) {
   let dateStamp = new Date();
   dateStamp.setTime(timestmp);
   $('#tstTime').text(`Results generated on: ${dateStamp.toLocaleString()}`)
 }
 
-function evenOutResults(){
+function evenOutResults() {
   //It looks funny when there are more results on the left side than right, or vice versa, so adjusting for that
 }
