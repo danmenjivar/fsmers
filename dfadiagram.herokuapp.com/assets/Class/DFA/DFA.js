2aa7b66a3dcda24ab46d39aea98a95f42826f161
Daniel Menjivar <daniel.quintanamenjivar882@myci.csuci.edu>
2020-04-23 14:01:43 -0700

    cleaned DFA

--- dfadiagram.herokuapp.com/assets/Class/DFA/DFA.js
+++ dfadiagram.herokuapp.com/assets/Class/DFA/DFA.js
@@ -35,29 +35,6 @@ class DFA {
 
 // Initial Sample DFA
 let DFATuples = {
-//   state: [],
-//   alphabet : [],
-//   final : [],
-//   initial : [],
-//   transition : {
-//     // q0 : {
-//     //   0 : 'q1',
-//     //   1 : 'q0'
-//     // },
-//     // q1 : {
-//     //   0 : 'q2',
-//     //   1 : 'q0'
-//     // },
-//     // q2 : {
-//     //   0 : 'q3',
-//     //   1 : 'q0'
-//     // },
-//     // q3 : {
-//     //   0 : 'q3',
-//     //   1 : 'q3'
-//     // }
-//   }
-// }
   state: ["q0","q1","q2",'q3'],
   alphabet : ['0','1'],
   final : ['q3'],
