ac765566ff536b58e5508da4a48ff1d8f95ae96f
Kyle Guieb <kyle.guieb285@myci.csuci.edu>
2020-04-23 15:47:41 -0700

    The basis of Transition handler, button set up

---
+++ dfadiagram.herokuapp.com/assets/Backend/TransitionHandler.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('#TransCommit').on('click', function () {
+        let toState = $('#toState').val();
+        let fromState = $('#fromState').value();
+      });
+
+});
